f76bb0d0b8fccef00b8022aba22f88d2b6dcc3f9
Troy Martin <beef.erikson.studios@gmail.com>
2019-09-16 16:19:07 -0400

    Initial commit

---
+++ Exercise1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter a score (-32,768 to 32,767): ");
+                short input = short.Parse(Console.ReadLine());
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Please use the proper format (integers only)");
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine("Out of integer limit - Enter a score in the bounds.");
+            }
+            finally
+            {
+                Console.WriteLine("Program run.");
+            }
+        }
+    }
+}
