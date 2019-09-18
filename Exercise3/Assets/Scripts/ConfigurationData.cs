5708cd0649a8d498ccabcfed86257b7a6cdb0235
Troy Martin <beef.erikson.studios@gmail.com>
2019-09-18 12:41:48 -0400

    Exercise 3 - Unity Project

---
+++ Exercise3/Assets/Scripts/ConfigurationData.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using UnityEngine;
+using System.IO;
+
+/// <summary>
+/// A container for the configuration data
+/// </summary>
+public class ConfigurationData
+{
+    #region Fields
+
+    const string ConfigurationDataFileName = "ConfigurationData.csv";
+
+    // configuration data with default values
+    static float teddyBearMoveUnitsPerSecond = 5;
+    static float cooldownSeconds = 1;
+
+    #endregion
+
+    #region Properties
+
+    /// <summary>
+    /// Gets the teddy bear move units per second
+    /// </summary>
+    /// <value>teddy bear move units per second</value>
+    public float TeddyBearMoveUnitsPerSecond
+    {
+        get { return teddyBearMoveUnitsPerSecond; }
+    }
+        
+    /// <summary>
+    /// Gets the cooldown seconds for shooting
+    /// </summary>
+    /// <value>cooldown seconds</value>
+    public float CooldownSeconds
+    {
+        get { return cooldownSeconds; }    
+    }
+
+    #endregion
+
+    #region Constructor
+
+    /// <summary>
+    /// Constructor
+    /// Reads configuration data from a file. If the file
+    /// read fails, the object contains default values for
+    /// the configuration data
+    /// </summary>
+    public ConfigurationData()
+    {
+        // read and save configuration data from file
+        StreamReader input = null;
+
+        try
+        {
+            input = File.OpenText(Path.Combine(Application.streamingAssetsPath, ConfigurationDataFileName));
+            
+            // reads names and values
+            string names = input.ReadLine();
+            string values = input.ReadLine();
+            
+            // sets configuration data values
+            SetConfigurationDataFields(values);
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e);
+        }
+        finally
+        {
+            input.Close();
+        }
+    }
+    
+    #endregion
+    
+    #region Methods
+
+    /// <summary>
+    /// Sets the configuration data fields from the provided
+    /// csv string
+    /// </summary>
+    /// <param name="csvValues">csv string of values</param>
+    static void SetConfigurationDataFields(string csvValues)
+    {
+        string[] values = csvValues.Split(',');
+        
+        // sets characteristics
+        teddyBearMoveUnitsPerSecond = float.Parse(values[0]);
+        cooldownSeconds = float.Parse(values[1]);
+    }
+
+    #endregion
+}
---
+++ Exercise3/Assets/Scripts/ConfigurationUtils.cs
@@ -0,0 +1,45 @@
+﻿/// <summary>
+/// Provides access to configuration data
+/// </summary>
+public static class ConfigurationUtils
+{
+    #region Fields
+
+    static ConfigurationData configurationData;
+    
+    #endregion
+    
+    #region Properties
+    
+    /// <summary>
+    /// Gets the teddy bear move units per second
+    /// </summary>
+    /// <value>teddy bear move units per second</value>
+    public static float TeddyBearMoveUnitsPerSecond
+    {
+        get { return configurationData.TeddyBearMoveUnitsPerSecond; }
+    }
+
+    /// <summary>
+    /// Gets the cooldown seconds for shooting
+    /// </summary>
+    /// <value>cooldown seconds</value>
+    public static float CooldownSeconds
+    {
+        get { return configurationData.CooldownSeconds; }    
+    }
+
+    #endregion
+    
+    #region Public Methods
+    
+    /// <summary>
+    /// Initializes the configuration utils
+    /// </summary>
+    public static void Initialize()
+    {
+        configurationData = new ConfigurationData();
+    }
+    
+    #endregion
+}
